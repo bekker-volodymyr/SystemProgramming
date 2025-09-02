76b0e90a006e52225b38856d448daa56977bc735
bekkerVU <bekker.volodymyr.yu@gmail.com>
2025-09-02 13:36:13 +0300

    task and async examples

---
+++ AsyncExamples/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+
+namespace AsyncExamples
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            #region Say Hello Async
+            var task = SayHelloAsync(); // Створюємо асинхронний виклик
+
+            await task; // очікуємо завершення виконання асинхронної операції
+
+            Console.WriteLine("Finish");
+            #endregion
+
+            #region Progress Bar
+
+            Task work = Task.Run(async () =>
+            {
+                await Task.Delay(3000);
+            });
+
+            while (!work.IsCompleted)
+            {
+                Console.Write(".");
+                await Task.Delay(300);
+            }
+
+            Console.WriteLine("\nDone!");
+
+            #endregion
+
+            #region Sync vs. Async
+
+            var sw = Stopwatch.StartNew();
+
+            Console.WriteLine($"Main запущено на потоці: {Thread.CurrentThread.ManagedThreadId}");
+
+            Task t1 = SimulateWorkAsync("Задача 1", 2000);
+            Task t2 = SimulateWorkAsync("Задача 2", 2000);
+            Task t3 = SimulateWorkAsync("Задача 3", 2000);
+
+            Console.WriteLine("Усі задачі запущено паралельно");
+
+            await Task.WhenAll(t1, t2, t3);
+
+            Console.WriteLine($"Час (асинхронно): {sw.Elapsed.TotalSeconds} сек");
+            Console.WriteLine("Усі задачі завершено");
+
+            sw = Stopwatch.StartNew();
+
+            await SimulateWorkAsync("Задача 1", 2000);
+            await SimulateWorkAsync("Задача 2", 2000);
+            await SimulateWorkAsync("Задача 3", 2000);
+
+            Console.WriteLine($"Час (по черзі): {sw.Elapsed.TotalSeconds} сек");
+
+            #endregion
+        }
+
+        static async Task SimulateWorkAsync(string name, int delayMs)
+        {
+            Console.WriteLine($"{name} стартувала на потоці {Thread.CurrentThread.ManagedThreadId}");
+            await Task.Delay(delayMs);
+            Console.WriteLine($"{name} завершилась на потоці {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        private static async Task SayHelloAsync()
+        {
+            Console.WriteLine("Working...");
+            await Task.Delay(3000);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TaskExamples/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace TaskExamples
+{
+    public class Program
+    {
+        private static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            #region Task Start
+            /*
+            Task task = new Task(SomeWork); // Створюємо асинхронну операцію
+            task.Start(); // Виконуємо асинхронну операцію
+
+            Console.WriteLine("Hello from Main!");
+
+            task.Wait(); // Очікуємо завершення асинхронної операції
+
+            Console.WriteLine("Finish");
+            */
+            #endregion
+
+            #region Task Run
+            /*
+            Task taskA = Task.Run(SomeWork); // Створюємо та запускаємо асинхронну операцію
+
+            taskA.Wait(); // Очікуємо завершення асинхронної операції
+            */
+            #endregion
+
+            #region WhenAll
+
+            //Task task1 = Task.Run(SomeWork);
+            //Task task2 = Task.Run(SomeWork);
+            //Task task3 = Task.Run(SomeWork);
+
+            //Task whenAll = Task.WhenAll(task1, task2, task3);
+
+            //whenAll.Wait();
+
+            //Console.WriteLine("All tasks have been completed.");
+
+            #endregion
+
+            #region WhenAny
+
+            //Task task4 = Task.Run(SomeWork);
+            //Task task5 = Task.Run(SomeWork);
+            //Task task6 = Task.Run(SomeWork);
+
+            //Task whenAny = Task.WhenAny(task4, task5, task6);
+
+            //whenAny.Wait();
+
+            //Console.WriteLine("One or more tasks have been completed.");
+
+            #endregion
+
+            #region Task with result
+
+            Task<int> taskI1 = new Task<int>(RandomNumber);
+            Task<int> taskI2 = new Task<int>(RandomNumber);
+            Task<int> taskI3 = new Task<int>(RandomNumber);
+
+            taskI1.Start();
+            taskI2.Start();
+            taskI3.Start();
+
+            int[] results = Task.WhenAll(taskI1, taskI2, taskI3).Result;
+
+            foreach (int result in results)
+            {
+                Console.WriteLine(result);
+            }
+
+            #endregion
+        }
+
+        private static int RandomNumber()
+        {
+            int result = random.Next(1000, 5000);
+            Task.Delay(result).Wait();
+            return result;
+        }
+
+        private static void SomeWork()
+        {
+            Console.WriteLine("Hello form task!");
+            Thread.Sleep(random.Next(1000, 5000));
+            Console.WriteLine("Task completed");
+        }
+    }
+}
