b678c6645b376a01b45c932acbf505a7047f9ca8
bekkerVU <bekker.volodymyr.yu@gmail.com>
2026-02-18 14:14:55 +0200

    interop examples

---
+++ 1_Legacy_Native_Code/PInvoke_Example/NativeMethods.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PInvoke_Example
+{
+    public static class NativeMethods
+    {
+        [DllImport("kernel32.dll")]
+        public static extern bool Beep(uint dwFreq, uint dwDuration);
+
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int printf(string format, __arglist);
+
+        [DllImport("user32.dll", CharSet=CharSet.Unicode)]
+        public static extern int MessageBox(IntPtr hWnd, string msg, string caption, uint type);
+
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "printf")]
+        public static extern int PrintLineFormat(string format, __arglist);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, ExactSpelling =true)]
+        public static extern int MessageBoxW(IntPtr hWnd, string msg, string caption, uint type);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr LoadLibrary(string dllToLoad);
+
+        public delegate bool EnumWC(IntPtr hwnd, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern int EnumWindows(EnumWC lpEnumFunc, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        public static bool OutputWindow(IntPtr hwnd, IntPtr lParam)
+        {
+            StringBuilder sb = new StringBuilder(256);
+
+            if (GetWindowText(hwnd, sb, sb.Capacity) > 0)
+            {
+                string windowName = sb.ToString();
+                Console.WriteLine($"HWND: {hwnd.ToInt64().ToString().PadRight(10)} | Title: {windowName}");
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 1_Legacy_Native_Code/PInvoke_Example/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+
+namespace PInvoke_Example
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            NativeMethods.Beep(1000, 1000);
+
+            NativeMethods.printf("Print params: %i %f", __arglist(10, 10.78));
+            NativeMethods.printf("\nPrint params: %i %s", __arglist(10, "abc"));
+
+            NativeMethods.MessageBox(new nint(0), "Hello, World!", "Message Box", 0);
+
+            NativeMethods.PrintLineFormat("\nPrint: %i %f %s", __arglist(12, 12.3, "hello"));
+
+            NativeMethods.MessageBoxW(new nint(0), "Привіт, світе!", "Повідомлення коробка", 1);
+
+            IntPtr handle = NativeMethods.LoadLibrary("fakelibrary.dll");
+            if(handle == IntPtr.Zero)
+            {
+                int errorCode = Marshal.GetLastWin32Error();
+                Console.WriteLine($"\nError {errorCode}");
+            }
+
+            NativeMethods.EnumWindows(NativeMethods.OutputWindow, IntPtr.Zero);
+        }
+    }
+}
